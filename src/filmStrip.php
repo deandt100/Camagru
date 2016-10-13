9bcc08356a76c12408e33cf3d6ac846394e1afcc
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-10-13 12:34:29 +0200

    a

--- src/filmStrip.php
+++ src/filmStrip.php
@@ -12,7 +12,8 @@
 	$stmt->execute();
 	$urls = $stmt->fetchAll(PDO::FETCH_COLUMN);
 	$stmt = $pdo->prepare("SELECT image_id FROM images 
-							ORDER BY date_created DESC");
+							WHERE user = '" . $user . "'" 
+							. "ORDER BY date_created DESC");
 	$stmt->execute();
 	$ids = $stmt->fetchAll(PDO::FETCH_COLUMN);
 	$pdo = null;
